2f94731df76f7ebeaa7fa59e2a6c9ceb912de4f3
Armel <armel.perhirin@orange.fr>
2016-12-10 15:19:29 +0100

    camera move update

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -5,64 +5,36 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour {
 
     public Transform player;
-    public float cameraSpeed = 0.1f;
-    public Transform topWall;
-    public Transform rightWall;
-    public Transform leftWall;
-    public Transform botWall;
-    private Vector2 screenSize; public float colDepth = 4f;
 
-    private float zPosition = 0f;
-    private Vector3 cameraPos;
-    private bool isOnSafeSpot;
-    private Vector3 cameraMove;
-    private bool move;
+    private bool goingLeft;
 
 	// Use this for initialization
 	void Start () {
-        cameraPos = transform.position;
-        cameraMove = new Vector3(- cameraSpeed, 0f, 0f);
-        isOnSafeSpot = false;
-        move = true;
-        screenSize.x = Vector2.Distance(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)), Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0))) * 0.5f;
-        screenSize.y = Vector2.Distance(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)), Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height))) * 0.5f;
-        rightWall.localScale = new Vector3(colDepth, screenSize.y * 2, colDepth);
-        rightWall.position = new Vector3(cameraPos.x + screenSize.x + (rightWall.localScale.x * 0.5f), cameraPos.y, zPosition);
-        leftWall.localScale = new Vector3(colDepth, screenSize.y * 2, colDepth);
-        leftWall.position = new Vector3(cameraPos.x - screenSize.x - (leftWall.localScale.x * 0.5f), cameraPos.y, zPosition);
-        topWall.localScale = new Vector3(screenSize.x * 2, colDepth, colDepth);
-        topWall.position = new Vector3(cameraPos.x, cameraPos.y + screenSize.y + (topWall.localScale.y * 0.5f), zPosition);
-        botWall.localScale = new Vector3(screenSize.x * 2, colDepth, colDepth);
-        botWall.position = new Vector3(cameraPos.x, cameraPos.y - screenSize.y - (botWall.localScale.y * 0.5f), zPosition);
+        goingLeft = true;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        Move();
-	}
-
-    void Move()
-    {
-        if (move)
+        if (goingLeft)
         {
-            if (isOnSafeSpot)
-            {
-                transform.position = new Vector3(player.position.x + 6.0f, 0f, -10.0f);
-            }
-            else
-            {
-                transform.position += cameraMove;
-            }
+            transform.position = new Vector3(player.position.x - 6.0f, 0f, -10.0f);
+        }
+        else
+        {
+            transform.position = new Vector3(player.position.x + 6.0f, 0f, -10.0f);
         }
     }
 
-    void StopCamera()
+    void ChangeDirection ()
     {
-        move = false;
+        if (goingLeft)
+        {
+            goingLeft = false;
+        }
+        else
+        {
+            goingLeft = true;
+        }
     }
 
-    void MoveCamera()
-    {
-        move = true;
-    }
 }
