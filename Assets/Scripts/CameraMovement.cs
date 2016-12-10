c201a17142a10eceea41f675fd61451e38e83179
Armel <armel.perhirin@orange.fr>
2016-12-10 16:42:46 +0100

    Cam update again

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -6,37 +6,16 @@ public class CameraMovement : MonoBehaviour {
 
     public Transform player;
 
-    private bool goingUp;
-
 	// Use this for initialization
 	void Start () {
-        goingUp = false;
+
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (goingUp)
-        {
-            transform.position = new Vector3(0f, player.position.y, -10.0f);
-        }
-        else
-        {
+            transform.position = new Vector3(player.position.x, player.position.y, -10.0f);
 
-            transform.position = new Vector3(player.position.x, 0f, -10.0f);
-
-        }
     }
 
-    void ChangeDirection ()
-    {
-        if (goingUp)
-        {
-            goingUp = false;
-        }
-        else
-        {
-            goingUp = true;
-        }
-    }
 
 }
