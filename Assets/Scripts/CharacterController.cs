f697db21feb94276681e85af2a0323b75b1e68f6
Hugo <hugoguillou@hotmail.fr>
2016-12-10 17:01:25 +0100

    clean character controller + wall jump

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -2,18 +2,17 @@ using System;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
-namespace UnityStandardAssets._2D
-{
-    [RequireComponent(typeof (PlatformerCharacter2D))]
-    public class Platformer2DUserControl : MonoBehaviour
+
+    [RequireComponent(typeof (Character))]
+    public class CharacterController : MonoBehaviour
     {
-        private PlatformerCharacter2D m_Character;
+        private Character m_Character;
         private bool m_Jump;
 
 
         private void Awake()
         {
-            m_Character = GetComponent<PlatformerCharacter2D>();
+            m_Character = GetComponent<Character>();
         }
 
 
@@ -23,7 +22,6 @@ namespace UnityStandardAssets._2D
             {
                 // Read the jump input in Update so button presses aren't missed.
                 m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
-                Debug.Log("JUMP " + m_Jump);
             }
         }
 
@@ -32,10 +30,20 @@ namespace UnityStandardAssets._2D
         {
             // Read the inputs.
             bool crouch = Input.GetKey(KeyCode.LeftControl);
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
+
+            int h = 0;
+
+            if(Input.GetKey(KeyCode.LeftArrow))
+                h = -1;
+
+            else if(Input.GetKey(KeyCode.RightArrow))
+                h = 1;
+
+            
             // Pass all parameters to the character control script.
             m_Character.Move(h, crouch, m_Jump);
+            
             m_Jump = false;
         }
     }
-}
+
