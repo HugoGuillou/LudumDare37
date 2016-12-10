d104046f197da5010fabdbeef7110a18250785b7
Armel <armel.perhirin@orange.fr>
2016-12-10 16:11:26 +0100

    Update camera

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -6,51 +6,37 @@ public class CameraMovement : MonoBehaviour {
 
     public Transform player;
 
-    private bool goingLeft;
     private bool goingUp;
 
 	// Use this for initialization
 	void Start () {
-        goingLeft = false;
         goingUp = false;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (goingLeft)
+        if (goingUp)
         {
-            transform.position = new Vector3(player.position.x - 6.0f, 0f, -10.0f);
+            transform.position = new Vector3(0f, player.position.y, -10.0f);
         }
         else
         {
-            if (goingUp)
-            {
-                transform.position = new Vector3(0f, player.position.y, -10.0f);
-            }
-            else
-            {
-                transform.position = new Vector3(player.position.x + 6.0f, 0f, -10.0f);
-            }
+
+            transform.position = new Vector3(player.position.x, 0f, -10.0f);
+
         }
     }
 
     void ChangeDirection ()
     {
-        goingUp = false;
-        if (goingLeft)
+        if (goingUp)
         {
-            goingLeft = false;
+            goingUp = false;
         }
         else
         {
-            goingLeft = true;
+            goingUp = true;
         }
-    }
-
-    void goUp()
-    {
-        goingUp = true;
-        goingLeft = false;
     }
 
 }
