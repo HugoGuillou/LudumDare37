1fcd3ae4036ba0ef22d3fcad8aa89146ec1d84c4
Armel <armel.perhirin@orange.fr>
2016-12-11 11:29:33 +0100

    switch cp

---
+++ Assets/Scripts/SwitchCheckPointBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCheckPointBehaviour : MonoBehaviour {
+
+    public GameObject checkpoint;
+    public LevelManager levelManager;
+
+    private bool sawOnce;
+
+    // Use this for initialization
+    void Start () {
+        levelManager = FindObjectOfType<LevelManager>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //affiche feedback
+            if (Input.GetButtonDown("Action"))
+            {
+                levelManager.currentCheckpoint = checkpoint;
+            }
+        }
+    }
+}
