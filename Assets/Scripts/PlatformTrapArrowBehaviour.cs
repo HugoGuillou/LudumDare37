4dc9a06df87bc75213a1f97fba2308b9d10d0ae6
Armel <armel.perhirin@orange.fr>
2016-12-11 18:33:56 +0100

    platformTrapArrow

---
+++ Assets/Scripts/PlatformTrapArrowBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformTrapArrowBehaviour : MonoBehaviour {
+
+    private Transform arrows;
+	// Use this for initialization
+	void Start () {
+		arrows = transform.GetChild(0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            arrows.GetComponent<ArrowBehaviour>().Activate();
+        }
+    }
+}
