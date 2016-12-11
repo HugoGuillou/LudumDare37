864e50e8aeb6efc696566f909b394c7cb1a936f5
Armel <armel.perhirin@orange.fr>
2016-12-11 12:31:41 +0100

    fix

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -22,12 +22,12 @@ public class LevelManager : MonoBehaviour {
     public void RespawnPlayer()
     {
         player.transform.position = currentCheckpoint.transform.position;
-        for(int i = 0; i <= laserWalls.Length; ++i)
+        for(int i = 0; i < laserWalls.Length; ++i)
         {
             laserWalls[i].Desactivate();
             laserWalls[i].SetPositionInitial();
         }
-        for (int j = 0; j <= lavaWalls.Length; ++j)
+        for (int j = 0; j < lavaWalls.Length; ++j)
         {
             lavaWalls[j].Desactivate();
             lavaWalls[j].SetPositionInitial();
