9a8ea5417d2de3029ec5325b36979187277d96bc
Armel <armel.perhirin@orange.fr>
2016-12-12 18:52:11 +0100

    Porte

---
+++ Assets/Scripts/DoorBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorBehaviour : MonoBehaviour {
+
+
+    private bool isIn;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Activate();
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            isIn = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isIn = false;
+        }
+    }
+
+    void Activate()
+    {
+        if(isIn && Input.GetButtonDown("Action"))
+        {
+            transform.GetChild(0).gameObject.SetActive(false);
+            transform.GetChild(1).gameObject.SetActive(true);
+        }
+    }
+
+}
